ba067fb211aaad26806506df1772dbc12f17f651
Michal <michal.erik.czajkowski@gmail.com>
2023-05-03 19:09:02 +0200

    add changing btn state with JS

--- src/pages/home-page/components/checkout-card-component/checkout-card-component.js
+++ src/pages/home-page/components/checkout-card-component/checkout-card-component.js
@@ -1,3 +1,20 @@
 export const CheckoutComponent = () => {
-  console.log("Hello from the checkout component");
+  //   console.log("Hello from the checkout component");
+  const checkoutCardComponent = document.querySelector(
+    ".checkout-card-component"
+  );
+  const confirmButton = document.querySelector(".btn-primary");
+  const cancelButton = document.querySelector(".btn-secondary");
+
+  confirmButton.addEventListener("click", () => {
+    confirmButton.setAttribute("data-loading", "1");
+    confirmButton.setAttribute("disabled", "disabled");
+    cancelButton.setAttribute("disabled", "disabled");
+  });
+
+  cancelButton.addEventListener("click", () => {
+    cancelButton.setAttribute("data-loading", "1");
+    cancelButton.setAttribute("disabled", "disabled");
+    confirmButton.setAttribute("disabled", "disabled");
+  });
 };
