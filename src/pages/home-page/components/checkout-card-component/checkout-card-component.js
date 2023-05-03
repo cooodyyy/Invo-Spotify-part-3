a0f948a8590215b682047a336e899fe36514836e
Michal <michal.erik.czajkowski@gmail.com>
2023-05-04 00:21:36 +0200

    add base logic

--- src/pages/home-page/components/checkout-card-component/checkout-card-component.js
+++ src/pages/home-page/components/checkout-card-component/checkout-card-component.js
@@ -6,14 +6,26 @@ export const CheckoutComponent = () => {
   const confirmButton = document.querySelector(".btn-primary");
   const cancelButton = document.querySelector(".btn-secondary");
 
+  let isLoading = false;
+
   confirmButton.addEventListener("click", () => {
+    if (isLoading) {
+      alert("The payment is already processing");
+      return;
+    }
     confirmButton.setAttribute("data-loading", "1");
+    isLoading = true;
     confirmButton.setAttribute("disabled", "disabled");
     cancelButton.setAttribute("disabled", "disabled");
   });
 
   cancelButton.addEventListener("click", () => {
+    if (isLoading) {
+      alert("The payment is already processing");
+      return;
+    }
     cancelButton.setAttribute("data-loading", "1");
+    isLoading = true;
     cancelButton.setAttribute("disabled", "disabled");
     confirmButton.setAttribute("disabled", "disabled");
   });
