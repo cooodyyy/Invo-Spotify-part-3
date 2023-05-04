17fcd8ba7cd5237bd381e091d109e3c4e2b27d82
Michal <michal.erik.czajkowski@gmail.com>
2023-05-04 23:05:29 +0200

    refactoring JS: exported function 'startLoading' and event delegation for buttons

--- src/pages/home-page/components/checkout-card-component/checkout-card-component.js
+++ src/pages/home-page/components/checkout-card-component/checkout-card-component.js
@@ -1,32 +1,19 @@
 export const CheckoutComponent = () => {
-  //   console.log("Hello from the checkout component");
-  const checkoutCardComponent = document.querySelector(
-    ".checkout-card-component"
-  );
-  const confirmButton = document.querySelector(".btn-primary");
-  const cancelButton = document.querySelector(".btn-secondary");
-
+  const buttons = document.querySelectorAll("button");
+  const buttonsSection = document.querySelector(".section-btns");
   let isLoading = false;
 
-  confirmButton.addEventListener("click", () => {
+  const startLoading = function (e) {
+    const clicked = e.target.closest("button");
+    if (clicked.hasAttribute("disabled")) return;
     if (isLoading) {
       alert("The payment is already processing");
       return;
     }
-    confirmButton.setAttribute("data-loading", "1");
+    clicked.setAttribute("data-loading", "1");
     isLoading = true;
-    confirmButton.setAttribute("disabled", "disabled");
-    cancelButton.setAttribute("disabled", "disabled");
-  });
+    buttons.forEach((btn) => btn.setAttribute("disabled", "disabled"));
+  };
 
-  cancelButton.addEventListener("click", () => {
-    if (isLoading) {
-      alert("The payment is already processing");
-      return;
-    }
-    cancelButton.setAttribute("data-loading", "1");
-    isLoading = true;
-    cancelButton.setAttribute("disabled", "disabled");
-    confirmButton.setAttribute("disabled", "disabled");
-  });
+  buttonsSection.addEventListener("click", startLoading);
 };
